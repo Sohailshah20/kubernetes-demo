8dd541804497f52bdefff876468c44e431dbff3d
sohailshah20 <contact.sohailali@gmail.com>
2025-01-25 18:33:53 +0530

    workflow

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text(`Hello Hono! ${process.env.MY_SECRET}`)
+  return c.text(`Hello Secret: ${process.env.MY_SECRET}`)
 })
 
-export default app
+export default {
+  port: process.env.SERVER_PORT || 3000,
+  fetch: app.fetch
+}
